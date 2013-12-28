007d6eba5b9bed9dfa09b9f63a70a5b9fb37bfd2
DucNM177 <RevolverKnight777@yahoo.com>
2013-12-28 20:45:12 +0700

    new test project

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by DucNM-Mac on 12/28/13.
+//  Copyright (c) 2013 DucNM-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGit/TestGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by DucNM-Mac on 12/28/13.
+//  Copyright (c) 2013 DucNM-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
